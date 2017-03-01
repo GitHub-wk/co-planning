85332ba05d9b5eace7aa201ac0b7cf53f2f977a1
weikang wei <weikang wei>
2017-03-01 19:15:37 +0800

    solve upload api

--- backend/restful/CONSTANT.js
+++ backend/restful/CONSTANT.js
@@ -7,8 +7,10 @@ var STATUS={
 	"REGISTER_SUCCESS":{lng:'注册成功',code:11},
 	"LOGIN_ERROR":{lng:"登陆错误",code:20},
 	"LOGIN_SUCCESS":{lng:"登陆成功",code:21},
-	"LOGOUT_SUCCESS":{lng:"注销成功",code:30},
-	"AUTH_ERROR":{lng:"验证失败",code:41},
+	"LOGOUT_SUCCESS":{lng:"注销成功",code:31},
+	"AUTH_ERROR":{lng:"验证失败",code:40},
+	"UPLOAD_SUCCESS":{lng:"上传成功",code:51},
+	"UPLOAD_ERROR":{lng:"上传失败",code:50},
 }
 var LNG={
 	
